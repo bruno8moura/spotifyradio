e8224fb6ffa57a7384868cbb193a622f7a2e8b5a
Bruno Moura <bruno8moura@gmail.com>
2022-03-15 05:31:18 -0300

    chore: add logs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import server from './server.js'
+import { logger } from './utils.js'
 
 server
 .listen(3000)
-.on('listening', () => console.log('server running'))
+.on('listening', () => logger.info('server running'))
