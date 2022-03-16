a59adaef283e25733e3dc108984b5243b6f37a78
Bruno Moura <bruno8moura@gmail.com>
2022-03-15 22:16:51 -0300

    feat: add route /home

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,43 @@
-export function handler(request, response) {
+import { logger } from './utils.js'
+
+import config from './config.js'
+import { Controller } from './Controller.js'
+
+const {
+    location,
+    pages: {
+        homeHTML,
+        controllerHTML
+    }
+} = config
+
+const controller = new Controller()
+async function routes(request, response) {
+    const { method, url } = request
+    if(method === 'GET' && url === '/') {
+        response.writeHead( 302, {
+            'Location': location.home
+        })
+
+        return response.end()
+    }
+
+    if(method === 'GET' && url === '/home') {
+        const {
+            stream,
+        } = await controller.getFileStream(homeHTML)
+
+        // response.writeHead( 200, {
+        //     'Content-Type': 'text/html'
+        // })
+
+        return stream.pipe(response)
+    }
+
     return response.end('hello')
+}
+
+export function handler(request, response) {
+    return routes(request, response)
+    .catch( error => logger.error(`Deu ruim: ${error.stack}`) && response.end())
 }
