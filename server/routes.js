df2b93095124b3244bcd2bc50667491684987b85
Bruno Moura <bruno8moura@gmail.com>
2022-03-16 04:53:05 -0300

    refactor: return right http code according what is happening

--- server/routes.js
+++ server/routes.js
@@ -58,7 +58,21 @@ async function routes(request, response) {
     return response.end()
 }
 
+function handleError(error, response) {
+    if(error.message.includes('ENOENT')) {
+     logger.warn(`asset not found ${error.stack}`)
+     response.writeHead(404)
+     
+     return response.end()
+    }
+
+    logger.error(`caught error on API ${error.stack}`)
+    response.writeHead(500)
+
+    return response.end()
+}
+
 export function handler(request, response) {
     return routes(request, response)
-    .catch( error => logger.error(`Deu ruim: ${error.stack}`) && response.end())
+    .catch( error => handleError(error, response))
 }
